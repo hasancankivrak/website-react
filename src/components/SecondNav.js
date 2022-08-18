a82108a7d023bbae1b86be83a41eba32253cb6b8
hasancankivrak <hasancankivrak@gmail.com>
2022-08-18 18:02:44 +0300

    Navbar için iyileştirme yapıldı

--- src/components/SecondNav.js
+++ src/components/SecondNav.js
@@ -8,18 +8,19 @@ import IgLogo from "../assets/iglogo.png";
 import LnLogo from "../assets/lnlogo.png";
 import Maillogo from "../assets/mail.png";
 import { Link } from "react-router-dom";
+import Menu from "../assets/menu.png";
 function Nav() {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
-      <nav className="bg-[#F2F2F2] rounded-b-[50px] z-[99] absolute lg:w-[1900px] lg:h-[65px]  sm:w-[350px] sm:h-[65px]">
+      <nav className="bg-[#F2F2F2] lg:rounded-b-[50px] sm:rounded-none z-[99] absolute lg:w-[1900px] lg:h-[65px]  sm:w-[360px] sm:h-[65px]">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center  lg:h-[104px] lg:w-[1500px] sm:h-[70px]">
             <div className="flex lg:space-x-[140px] lg:mt-[-20px] sm:space-x-[10px]">
               <div className="flex-shrink-0 lg:ml-[-890px] lg:mt-[-20px]">
                 <Link to="/">
                   <img
-                    className="lg:h-[40px] lg:w-[150px] lg:mt-[5px] lg:ml-[650px] sm:h-[30px] sm:ml-[10px]"
+                    className="lg:h-[40px] lg:w-[150px] lg:mt-[5px] lg:ml-[650px] sm:h-[30px] sm:ml-[110px]"
                     src={Logo}
                     alt="logo"
                   /></Link>
@@ -71,24 +72,20 @@ function Nav() {
                 onClick={() => setIsOpen(!isOpen)}
                 type="button"
                 className="
-                bg-gray-900 inline-flex items-center 
-                justify-center p-2 rounded-md text-gray-400 hover:text-white 
-                hover:bg-gray-800 focus:outline-none focus:ring-2 
-                focus:ring-offset-2 focus:ring-offset-gray-800 
-                focus:ring-white ml-[110px]"
+                bg-white inline-flex items-center 
+                justify-center p-2 rounded-md text-gray-400  
+                 ml-[-240px]"
                 aria-controls="mobile-menu"
                 aria-expanded="false"
               >
                 <span className="sr-only">Open main menu</span>
                 {!isOpen ? (
-                  <svg
-                    className="block h-[15px] w-[15px]"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
+                  <svg xmlns="http://www.w3.org/2000/svg" width="34.215" height="24.987" viewBox="0 0 34.215 24.987">
+                    <g id="Group_420" data-name="Group 420" transform="translate(-7.595 -13.399)">
+                      <line id="Line_8" data-name="Line 8" x2="31.215" transform="translate(9.095 25.893)" fill="none" stroke="#1facd7" stroke-linecap="round" stroke-miterlimit="10" stroke-width="3" />
+                      <line id="Line_9" data-name="Line 9" x2="14.924" transform="translate(9.095 14.899)" fill="none" stroke="#333" stroke-linecap="round" stroke-miterlimit="10" stroke-width="3" />
+                      <line id="Line_10" data-name="Line 10" x2="14.924" transform="translate(25.386 36.886)" fill="none" stroke="#333" stroke-linecap="round" stroke-miterlimit="10" stroke-width="3" />
+                    </g>
                     <path
                       strokeLinecap="round"
                       strokeLinejoin="round"
@@ -97,14 +94,12 @@ function Nav() {
                     />
                   </svg>
                 ) : (
-                  <svg
-                    className="block h-[15px] w-[15px]"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
+                  <svg xmlns="http://www.w3.org/2000/svg" width="34.215" height="24.987" viewBox="0 0 34.215 24.987">
+                    <g id="Group_420" data-name="Group 420" transform="translate(-7.595 -13.399)">
+                      <line id="Line_8" data-name="Line 8" x2="31.215" transform="translate(9.095 25.893)" fill="none" stroke="#1facd7" stroke-linecap="round" stroke-miterlimit="10" stroke-width="3" />
+                      <line id="Line_9" data-name="Line 9" x2="14.924" transform="translate(9.095 14.899)" fill="none" stroke="#333" stroke-linecap="round" stroke-miterlimit="10" stroke-width="3" />
+                      <line id="Line_10" data-name="Line 10" x2="14.924" transform="translate(25.386 36.886)" fill="none" stroke="#333" stroke-linecap="round" stroke-miterlimit="10" stroke-width="3" />
+                    </g>
                     <path
                       strokeLinecap="round"
                       strokeLinejoin="round"
@@ -128,8 +123,8 @@ function Nav() {
           leaveTo="opacity-0 scale-95"
         >
           {(ref) => (
-            <div className="md:hidden text-left items-center  bg-white rounded-[20px] mt-[-4px]" id="mobile-menu">
-              <div ref={ref} className="ml-[50px] px-2 pt-2 pb-3 space-y-1 sm:px-3 ">
+            <div className="md:hidden text-left w-[200px] h-[240px] bg-[#F2F2F2] rounded-br-[30px] mt-[-5px]" id="mobile-menu">
+              <div ref={ref} className="ml-[20px] px-2 pt-2 pb-3 space-y-[15px] sm:px-3 ">
                 <Link to="/">
                   <p
                     className="text-black mr-[500px] text-[15px] hover:underline"
@@ -139,7 +134,7 @@ function Nav() {
                 <Link to="/">
                   <p
 
-                    className="text-black mr-[500px] text-[15px] hover:underline"
+                    className="text-black ml-[px] text-[15px] hover:underline mt-[10px]"
                   >
                     ABOUT US
                   </p>
@@ -147,25 +142,25 @@ function Nav() {
                 <Link to="/">
                   <p
 
-                    className="text-black mr-[500px] text-[15px] hover:underline"
+                    className="text-black mr-[500px] text-[15px] hover:underline mt-[10px]"
                   >
                     PRODUCTS
                   </p>
                 </Link>
                 <Link to="#">
                   <p
-                    className="text-black mr-[500px] text-[15px] hover:underline"
+                    className="text-black mr-[500px] text-[15px] hover:underline mt-[10px]"
                   >
                     CAREER
                   </p></Link>
-                 <Link to="/">
-                <button
-                  className="bg-[#1AADD8] h-[65px] mr-[500px] w-[150px] text-[15px] text-white hover:text-black font-bold py-2 px-4 rounded-full"
-                >
-                  CONTACT US
-                </button></Link>
+                <Link to="/">
+                  <button
+                    className="bg-[#1AADD8] h-[45px] ml-[-20px] w-[140px] text-[15px] text-black hover:text-black font-bold py-2 px-4 rounded-full mt-[10px]"
+                  >
+                    CONTACT US
+                  </button></Link>
                 <div className="flex flex-row ml-[-10px]">
-                <a href="mailto:hasancankivrak@gmail.com"><img src={Maillogo} alt="mail"></img></a>
+                  <a href="mailto:hasancankivrak@gmail.com"><img src={Maillogo} alt="mail"></img></a>
                   <img src={LnLogo} alt="Linkedin"></img>
                 </div>
               </div>
